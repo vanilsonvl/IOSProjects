23edbe592d871e823a7849aa37364d51d6c44170
Vanilson Leite <vanilson.leite@palotec.com.br>
2017-04-06 10:32:36 -0300

    First Commit

---
+++ App01CourseIOS/App01CourseIOS/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  App01CourseIOS
+//
+//  Created by Palotec on 04/04/17.
+//  Copyright © 2017 Vanilsonvl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var ageField: UITextField!
+    @IBOutlet weak var resultText: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func getAge(_ sender: Any) {
+        resultText.text = "A idade do cachorro é " + String(Int(ageField.text!)! * 7)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
